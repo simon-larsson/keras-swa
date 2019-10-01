59ca50651dde0cbffed3cf2eb5fee2ea00bacabe
Simon Larsson <simon.larsson@seavus.com>
2019-10-01 19:24:56 +0200

    added implementation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='keras_swa',
+    version='0.0.1',
+    description='Simple stochastic weight averaging callback for Keras.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    author='Simon Larsson',
+    author_email='simonlarsson0@gmail.com',
+    url='https://github.com/simon-larsson/extrakit-learn',
+    license='MIT',
+    install_requires=[],
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Topic :: Scientific/Engineering']
+)
---
+++ swa/tfkeras.py
@@ -0,0 +1,55 @@
+""" TF-Keras SWA: callback utility for performing stochastic weight averaging (SWA).
+"""
+
+from tensorflow.keras.callbacks import Callback
+
+class SWA(Callback):
+    """ Stochastic Weight Averging.
+    
+    # Paper
+        title: Averaging Weights Leads to Wider Optima and Better Generalization
+        link: https://arxiv.org/abs/1803.05407
+        authors: Pavel Izmailov, 
+                 Dmitrii Podoprikhin, 
+                 Timur Garipov, 
+                 Dmitry Vetrov, 
+                 Andrew Gordon Wilson
+    
+    # Arguments
+        swa_epochs: integer, the number epochs swa will be applied to.
+                    first swa epoch will be epochs - swa_epochs.
+        verbose: integer, verbosity mode, 0 or 1.
+    """
+    def __init__(self, swa_epochs, verbose=0):
+        super(SWA, self).__init__()
+        self.swa_epochs = swa_epochs
+        self.verbose = verbose
+            
+    def on_train_begin(self, logs=None):
+        self.epochs = self.params.get('epochs')
+
+        if self.swa_epochs < 1:
+            raise ValueError('"swa_epochs" attribute cannot be lower than 1.')
+        
+        if self.swa_epochs >= self.epochs:
+            raise ValueError('"swa_epochs" attribute must be lower than "epochs".')
+
+        self.start_epoch = self.epochs - self.swa_epochs
+    
+    def on_epoch_end(self, epoch, logs=None):
+        if epoch == self.start_epoch:
+            self.weights = self.model.get_weights()
+            
+            if self.verbose > 0:
+                print('\nEpoch %05d: starting stochastic weight averaging'
+                      % (epoch + 1))
+        
+        if epoch >= self.start_epoch:
+            self.weights = [(weight * (epoch - self.start_epoch) + self.model.get_weights()[i]) / 
+                            ((epoch - self.start_epoch) + 1) for i, weight in enumerate(self.weights)]
+                           
+    def on_train_end(self, logs=None):
+        self.model.set_weights(self.weights)
+        if self.verbose > 0:
+            print('\nEpoch %05d: final model weights set to stochastic weight average'
+                  % (self.epochs))
