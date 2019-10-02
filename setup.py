7b90bc14f5a94703f60504475cf5da9f204bc1fb
Simon Larsson <simon.larsson@seavus.com>
2019-10-02 13:39:27 +0200

    fixed pypi name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name='keras_swa',
+    name='keras-swa',
     version='0.0.1',
     description='Simple stochastic weight averaging callback for Keras.',
     long_description=long_description,
