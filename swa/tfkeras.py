4cb94f82a684dc3d58c10358781c9fbb0649ec1b
Simon Larsson <simon.larsson@seavus.com>
2019-10-14 18:02:57 +0200

    managed to remove batch_size

--- swa/tfkeras.py
+++ swa/tfkeras.py
@@ -16,17 +16,14 @@ class SWA(Callback):
         start_epoch:   integer, epoch when swa should start.
         lr_schedule:   string, type of learning rate schedule.
         swa_lr:        float, learning rate for swa.
-        batch_size:    integer, training batch size, not always required.
         verbose:       integer, verbosity mode, 0 or 1.
     """
-    def __init__(self, start_epoch, lr_schedule=None, 
-                 swa_lr=0.001, batch_size=None, verbose=0):
+    def __init__(self, start_epoch, lr_schedule=None, swa_lr=0.001, verbose=0):
         
         super(SWA, self).__init__()
         self.start_epoch = start_epoch - 1
         self.lr_schedule = lr_schedule
         self.swa_lr = swa_lr
-        self.batch_size = batch_size
         self.verbose = verbose
         
         if start_epoch < 2:
@@ -45,9 +42,6 @@ class SWA(Callback):
         
         self.epochs = self.params.get('epochs')
         
-        if self.params.get('batch_size') is not None:
-            self.batch_size = self.params.get('batch_size')
-        
         if self.start_epoch >= self.epochs - 1:
             raise ValueError('"swa_start" attribute must be lower than "epochs".')
 
@@ -57,10 +51,6 @@ class SWA(Callback):
             raise ValueError('"swa_lr" must be lower than rate set in optimizer.')
             
         self._check_batch_norm()
-        
-        if self.has_batch_norm and self.batch_size is None:
-            raise ValueError('"batch_size" has to be set manually for models with ' 
-                             'batch normalization when fitting with generators.')
 
     def on_epoch_begin(self, epoch, logs=None):
         
@@ -86,7 +76,9 @@ class SWA(Callback):
         
         if self.running_bn_epoch:
             
-            momentum = self.batch_size / (batch*self.batch_size + self.batch_size)
+            batch_size = self.params.get('samples')
+            
+            momentum = batch_size / (batch*batch_size + batch_size)
 
             for layer in self.batch_norm_layers:
                 layer.momentum = momentum
