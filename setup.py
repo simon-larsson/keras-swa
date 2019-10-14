96c70b11aba3f862a3cf352246fd63be38818a45
Simon Larsson <simon.larsson@seavus.com>
2019-10-14 18:03:50 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='keras-swa',
-    version='0.0.4',
+    version='0.0.5',
     description='Simple stochastic weight averaging callback for Keras.',
     long_description=long_description,
     long_description_content_type='text/markdown',
